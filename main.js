4eee2bba9e54651c4eca7e66012bbda27f3c9164
Atharv-khandelwal <83112922+Atharv-khandelwal@users.noreply.github.com>
2021-05-21 17:05:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,78 @@
-song=''
-leftWristX=0
-leftWristY=0
-rightWristX=0
-rightWristY=0
-function preload(){
-    song= loadSound('believer_remix.mp3')
+scoreLeftWrist = 0
+ScoreRightWrist = 0
+status = ""
+status2 = ""
+song1 = ""
+song = ''
+leftWristX = 0
+leftWristY = 0
+rightWristX = 0
+rightWristY = 0
+function preload() {
+    song = loadSound('believer_remix.mp3')
+    song1 = loadSound('music.mp3')
 }
 
-function setup(){
-    canvas=createCanvas(500,600)
-    canvas.center
+function setup() {
+    canvas = createCanvas(500, 600)
+    canvas.center()
 
-    video= createCapture(VIDEO)
+    video = createCapture(VIDEO)
     video.hide()
 
-    poseNet=ml5.posenet(video, modelLoaded)
+    poseNet = ml5.poseNet(video, modelLoaded)
     poseNet.on('pose', gotPoses)
 }
 
-function draw(){
-    image(video,0,0, 600,500)
-}
+function draw() {
+    image(video, 0, 0, 600, 500)
+    status = song.isPlaying()
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20)
+        song1.stop()
+        if (status == false) {
+            song.play()
+            document.getElementById('songName').innerHTML = "Playing Beliver"
+        }
+    }
 
-function play(){
-    song.play()
-    song.setVolume(1)
-    song.rate(1)
+        if (scorerightWrist > 0.2) {
+            circle(rightWristX, rightWristY, 20)
+            song.stop()
+            if (status2 == false) {
+                song1.play()
+                document.getElementById('songName').innerHTML = "Playing ringtone"
+            }
 
-}
 
-function modelLoaded(){
-    console.log('PoseNet Is Initialized')
 
-}
+        }
+    }
 
-function gotPoses(results){
-    if(results>0){
-        console.log(results)
+    function play() {
+        song.play()
+        song.setVolume(1)
+        song.rate(1)
 
-leftWristX=results[0].pose.leftWrist.x
-leftWristY=results[0].pose.leftWrist.y
-console.log('leftWristX'+ leftWristX, 'leftWristY'+ leftWristY)
+    }
 
-rightWristX=results[0].pose.rightWrist.x
-rightWristY=results[0].pose.leftWrist.y
-console.log('rightWristX'+rightWristX, 'rightWristY'+rightWristY)    
-}
-}
+    function modelLoaded() {
+        console.log('PoseNet Is Initialized')
+
+    }
+
+    function gotPoses(results) {
+        if (results > 0) {
+            console.log(results)
+
+            leftWristX = results[0].pose.leftWrist.x
+            leftWristY = results[0].pose.leftWrist.y
+            console.log('leftWristX' + leftWristX, 'leftWristY' + leftWristY)
+            scoreLeftWrist = results[0].pose.keypoints[9].score
+
+            ScoreRightWrist = results[0].pose.keypoints[10].score
+            rightWristX = results[0].pose.rightWrist.x
+            rightWristY = results[0].pose.leftWrist.y
+            console.log('rightWristX' + rightWristX, 'rightWristY' + rightWristY)
+        }
+    }
